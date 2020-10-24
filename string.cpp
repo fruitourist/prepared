5d1ce6b8d9cf886f60ed42cb3c70ba0b94092dfb
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-10-24 11:58:36 +0400

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -11,7 +11,7 @@ string lowerCase(string s) {
 	return s;
 }
 
-map<char, string> morseMapAlphabet {
+map<char, string> morseMapAlphabet = {
 	{'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."},
 	{'g', "--."}, {'h', "...."}, {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."},
 	{'m', "--"}, {'n', "-."}, {'o', "---"}, {'p', ".--."}, {'q', "--.-"}, {'r', ".-."},
