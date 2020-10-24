10d04a892ff26e04200433c649d7c5f6a1965784
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-10-24 11:37:44 +0400

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 string lowerCase(string s) {
 	char c;
@@ -10,6 +11,22 @@ string lowerCase(string s) {
 	}
 	
 	return s;
+}
+
+map<char, string> morseMapAlphabet {
+	{'a', ".-"}, {'b', "-..."}, {'c', "-.-."}, {'d', "-.."}, {'e', "."}, {'f', "..-."},
+	{'g', "--."}, {'h', "...."}, {'i', ".."}, {'j', ".---"}, {'k', "-.-"}, {'l', ".-.."},
+	{'m', "--"}, {'n', "-."}, {'o', "---"}, {'p', ".--."}, {'q', "--.-"}, {'r', ".-."},
+	{'s', "..."}, {'t', "-"}, {'u', "..-}, {'v', "...-"}, {'w', ".--"}, {'x', "-..-"},
+	{'y', "-.--"}, {'z', "--.."}
+}
+
+string morse(string word) {
+	word = lowerCase(word);
+	for (int i = 0; i < (int)word.length(); i++)
+		word[i] = morseMapAlphabet[word[i]];
+	
+	return word;
 }
 
 int main() {
